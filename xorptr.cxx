46c9b617f9da29522bdded0b6844249b43dfc139
Jonathan Almeida <git@jonalmeida.com>
2016-04-03 19:55:17 -0400

    Added soome unfinished rough test case

--- xorptr.cxx
+++ xorptr.cxx
@@ -5,10 +5,14 @@ int main() {
   int a = 256;
   int b = 255;
   int c = 3;
+  int *a_ptr = &a;
+  int *b_ptr = &b;
   int *c_ptr = &c;
   auto foo = xorptr<int>(&a, &b);
-  std::cout << &a << " " << &b << " ";
-  std::cout << (foo ^ c_ptr) << std::endl;
+  std::cout << "&a -> " << &a << " &b -> " << &b << "\n";
+  std::cout << "&foo: " << (&foo) << "\n";
+  std::cout << "(foo ^ a_ptr): " << (foo ^ a_ptr) << std::endl;
+  std::cout << "(foo ^ b_ptr): " << (foo ^ b_ptr) << std::endl;
 }
 
 
