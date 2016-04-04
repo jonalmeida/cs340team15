685552ffb77e5b0ded44cf551387c09489a473d5
Jonathan Almeida <git@jonalmeida.com>
2016-04-04 16:12:19 -0400

    Adding a simple test

--- dllist.cxx
+++ dllist.cxx
@@ -1,5 +1,15 @@
 #include "dllist.hxx"
+#include <initializer_list>
+#include <iostream>
 
 int main() {
   std::cout << "hello, world!" << std::endl;
+  dllist<int> tester( {1, 2, 3 } );
+  //l.push_back(10);
+  tester.push_front(2);
+  tester.push_back(2);
+  tester.pop_back();
+  for (auto i : tester){
+    std::cout << i << ' ';
+  }
 }
